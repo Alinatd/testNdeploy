f03f2d79f9d7cf10f5a69b4b5d9d211dc0874ee1
Alina Turdakunova <turdakunova@Alinas-MacBook-Pro.local>
2023-12-23 22:47:14 +0600

    first commitgit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Alina</h1>
+  <button>Alina</button>
+  <input> .
+  </input>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div style={{width:'200px',background:'red'}}>
+      
+    </div>
+  )
+}
+
+export default index
